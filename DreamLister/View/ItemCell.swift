38d061fa51c9876235b0cc790d5b03928bca6a0b
shiv anand <shivakp2111@gmail.com>
2018-06-20 12:42:23 +0530

    some boilerplate Codes

---
+++ DreamLister/View/ItemCell.swift
@@ -0,0 +1,30 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by apple on 20/06/18.
+//  Copyright © 2018 shiv. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+}

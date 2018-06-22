d061db8d3944d72efcf1828c1cce4b90dc836f47
shiv anand <shivakp2111@gmail.com>
2018-06-22 11:19:10 +0530

    uipickerView delegate methods

--- DreamLister/Controller/ItemDetailsVC.swift
+++ DreamLister/Controller/ItemDetailsVC.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ItemDetailsVC: UIViewController {
+class ItemDetailsVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleField: CustomTextField!
+    @IBOutlet weak var priceField: CustomTextField!
+    @IBOutlet weak var detailsField: CustomTextField!
+    
+    // hold our stores
+    var stores = [Store]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         if let topItem = self.navigationController?.navigationBar.topItem {
             topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.done, target: nil, action: nil)
         }
+        
+        // delegates and data source
+        storePicker.delegate = self
+        storePicker.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let store = stores[row]
+        return store.name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
     }
 
 }
